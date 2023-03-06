aa9bd529d73dfa1df59c765aea121d8f9bb02e85
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-06 15:41:18 +0530

    added components like text and buttons

--- ReactNative/DemoApp/App.js
+++ ReactNative/DemoApp/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+// view is equivalent to div section 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>"Hello instead " 
       </Text>
+      <View>
+      <Text>Another piece of text</Text>
+      </View>
+      <Button title="Tap me daddy"/>
       <StatusBar style="auto" />
     </View>
   );
