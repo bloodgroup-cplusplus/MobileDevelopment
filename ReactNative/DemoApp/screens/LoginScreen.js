b034967f73492db6e9b755983dfd55363339b371
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-06 19:38:24 +0530

    added styles and some compnent called <keyboardAvoidingView>

--- ReactNative/DemoApp/screens/LoginScreen.js
+++ ReactNative/DemoApp/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet,Text,View } from "react-native";
+import { KeyboardAvoidingView, StyleSheet,Text,View } from "react-native";
 import {Button,Input,Image} from "react-native-elements"
 import signal_logo from "../assets/signal_logo.png"
 import { useState } from "react";
@@ -9,15 +9,25 @@ import { useState } from "react";
 const LoginScreen = () =>{
     const[email,setEmail] = useState("")
     const[password,setPassword] = useState("")
+
+    const signIn=()=>{};
     return (
         <View>
+            <KeyboardAvoidingView>
             <StatusBar stlye="light"/>
             <Image source={signal_logo}
             style={{width:200,height:200}}
             />
-            <View style={styles.inputContainer}></View>
+            <View style={styles.inputContainer}>
             <Input placeholder="Email" autoFocus type="Email" value={email} onChangeText={text=>setEmail(text)}/>
-            <Input placeholder="Password" secureTextEntry type="password" value={password} onChangeText={text=>setPassword(password)}/>
+            <Input placeholder="Password" secureTextEntry type="password" value={password} onChangeText={text=>setPassword(text)}/>
+            </View>
+            <Button containerStyle={styles.button} onPress={signIn} title="Login"/>
+            <Button containerStyle={styles.button} type="outline" title="Register"/>
+            <View style={{height:100}}/>
+            </KeyboardAvoidingView>
+
+
         </View>
     );
 };
@@ -28,6 +38,13 @@ export default LoginScreen;
 
 
 const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center",
+        padding:10,
+        backgroundColor:"white",
+    },
     inputContainer:{
 
     }
