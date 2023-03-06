8e6846c5c27c9da9f0edd06625b8cb21b88bc2ab
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-06 16:26:35 +0530

    added react navigation module read react-native-navigation router for more info

--- ReactNative/DemoApp/App.js
+++ ReactNative/DemoApp/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 // view is equivalent to div section 
 
 export default function App() {
