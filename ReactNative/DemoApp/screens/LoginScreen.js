8dc6a4bdb84e63a3118559e897a59e5a7d331eaf
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-03-06 19:28:19 +0530

    use state for email and password field in react native

--- ReactNative/DemoApp/screens/LoginScreen.js
+++ ReactNative/DemoApp/screens/LoginScreen.js
@@ -3,15 +3,21 @@ import React from "react";
 import { StyleSheet,Text,View } from "react-native";
 import {Button,Input,Image} from "react-native-elements"
 import signal_logo from "../assets/signal_logo.png"
+import { useState } from "react";
 
 
 const LoginScreen = () =>{
+    const[email,setEmail] = useState("")
+    const[password,setPassword] = useState("")
     return (
         <View>
             <StatusBar stlye="light"/>
             <Image source={signal_logo}
             style={{width:200,height:200}}
             />
+            <View style={styles.inputContainer}></View>
+            <Input placeholder="Email" autoFocus type="Email" value={email} onChangeText={text=>setEmail(text)}/>
+            <Input placeholder="Password" secureTextEntry type="password" value={password} onChangeText={text=>setPassword(password)}/>
         </View>
     );
 };
@@ -21,4 +27,8 @@ export default LoginScreen;
 
 
 
-const styles= StyleSheet.create({});
+const styles= StyleSheet.create({
+    inputContainer:{
+
+    }
+});
